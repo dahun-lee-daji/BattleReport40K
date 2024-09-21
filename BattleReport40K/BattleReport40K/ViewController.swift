9a15183a2023c358e2ef0fe2f1f1d7c7529dc9e0
dahun <ssss6596@gmail.com>
2024-09-21 17:40:03 +0900

    init Proj. with UIKit

---
+++ BattleReport40K/BattleReport40K/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BattleReport40K
+//
+//  Created by 이다훈 on 9/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
